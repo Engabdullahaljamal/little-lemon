77fef067ff1fdb620450274fe21adf88e4958eac
abdullah <en>
2024-09-15 22:18:14 +0300

    new update

--- src/main.jsx
+++ src/main.jsx
@@ -6,8 +6,8 @@ import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
+    <BrowserRouter basename='/little-lemon/'>
+      <App  />
     </BrowserRouter>
   </StrictMode>,
 )
