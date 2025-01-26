1630b364942b64106c2d6feb81d23c4d016a3922
abdullah <en>
2025-01-26 15:54:06 -0500

    new update

--- src/Pages/Reservation/Reservation.jsx
+++ src/Pages/Reservation/Reservation.jsx
@@ -10,15 +10,15 @@ function Reservation() {
             <h3 className='title-text-center' >TABLE RESERVATION</h3>
             <form>
                 <div className='inputs_container'>
-                    <label htmlFor="full_name">Full Name</label>
+                    <label htmlFor="full_name">Full Name : </label>
                     <input type="text" id="full_name" />
-                    <label htmlFor="date">Date</label>
+                    <label htmlFor="date">Date :</label>
                     <input type="date" id="date" />
                 </div>
                 <div className='inputs_container'>
-                    <label htmlFor="time">Time</label>
+                    <label htmlFor="time">Time :</label>
                     <input type="time" id="time" />
-                    <label htmlFor="number">Number of guests</label>
+                    <label htmlFor="number">Number of guests :</label>
                     <input type="number" id="number" />
 
                 </div>
